5ecb88b097eafeb465a75e72886c659841bef978
byplayer <byplayer100@gmail.com>
2014-05-14 13:04:52 +0800

    update tmux version

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@
 # tmux
 default['tmux']['install_method'] = 'source'
 default['tmux']['configure_options'] = %w(--prefix=/opt/tmux)
-default['tmux']['version'] = '1.9'
+default['tmux']['version'] = '1.9a'
