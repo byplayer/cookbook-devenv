47d4ec01dc6227897c3b4491f3a77601fb5a8319
byplayer <byplayer100@gmail.com>
2014-07-22 17:28:41 +0800

    add packages

--- recipes/_packages.rb
+++ recipes/_packages.rb
@@ -1,4 +1,20 @@
-%w(zsh).each do |pkg|
+%w(lubuntu-desktop zsh autotools-dev automake libtool
+  libevent-dev libncurses5-dev zlib1g-dev libbz2-dev libyaml-dev
+  libxml2-dev libxslt1-dev libreadline-dev xclip patch
+  chromium-browser
+  ibus-mozc mozc-server mozc-utils-gui
+  exuberant-ctags
+  emacs24 emacsen-common ibus-el rdtool-elisp emacs-goodies-el
+  xfonts-shinonome python-xlib
+  libterm-readkey-perl
+  mlocate ssh
+  libsqlite3-dev sqlite3
+  expect dkms
+  ia32-libs
+  silversearcher-ag
+  eblook lookup-el
+  libgecode-dev
+).each do |pkg|
   apt_package pkg do
     action :install
   end
