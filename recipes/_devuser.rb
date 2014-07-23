dbebfc8e4dc3cc887b0ae68575bf90f7e0f89530
byplayer <byplayer100@gmail.com>
2014-07-23 13:30:12 +0800

    fix bug

--- recipes/_devuser.rb
+++ recipes/_devuser.rb
@@ -1,6 +1,6 @@
 user node['devenv']['user']['name'] do
   shell node['devenv']['user']['shell']
-  password ['devenv']['user']['password']
+  password node['devenv']['user']['password']
   home "/home/#{node['devenv']['user']['name']}"
   supports manage_home: true
 end
