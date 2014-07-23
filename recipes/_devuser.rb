9d1c02e121b9ad286c7d9991b3a4d557a088077b
byplayer <byplayer100@gmail.com>
2014-07-23 15:53:36 +0800

    add .zsh.d recipe

--- recipes/_devuser.rb
+++ recipes/_devuser.rb
@@ -1,6 +1,15 @@
+devenv_user_home = "/home/#{node['devenv']['user']['name']}"
+
 user node['devenv']['user']['name'] do
   shell node['devenv']['user']['shell']
   password node['devenv']['user']['password']
-  home "/home/#{node['devenv']['user']['name']}"
+  home devenv_user_home
   supports manage_home: true
+end
+
+# .zsh.d
+git "#{devenv_user_home}/.zsh.d" do
+  repository node['devenv']['zshd']['repo']
+  reference node ['devenv']['zshd']['ref']
+  action :sync
 end
--- recipes/_packages.rb
+++ recipes/_packages.rb
@@ -1,4 +1,5 @@
-%w(lubuntu-desktop zsh autotools-dev automake libtool
+%w(aptitude
+  lubuntu-desktop zsh autotools-dev automake libtool
   libevent-dev libncurses5-dev zlib1g-dev libbz2-dev libyaml-dev
   libxml2-dev libxslt1-dev libreadline-dev xclip patch
   chromium-browser
