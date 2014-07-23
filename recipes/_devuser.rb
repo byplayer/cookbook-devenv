2b311e8ae4a87042773af77e7c7780c1a2330d5f
byplayer <byplayer100@gmail.com>
2014-07-23 16:22:50 +0800

    update repository

--- recipes/_devuser.rb
+++ recipes/_devuser.rb
@@ -9,7 +9,7 @@ end
 
 # .zsh.d
 git "#{devenv_user_home}/.zsh.d" do
-  remote node['devenv']['zshd']['repo']
+  repository node['devenv']['zshd']['repo']
   reference node['devenv']['zshd']['ref']
   action :sync
 end
