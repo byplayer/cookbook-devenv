34dd0e13feb888a41bed5b493bbfdba4689c2104
byplayer <byplayer100@gmail.com>
2014-07-23 15:55:58 +0800

    attribute for .zsh.d

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,9 @@ default['devenv']['user']['name'] = 'devuser'
 default['devenv']['user']['shell'] = '/usr/bin/zsh'
 default['devenv']['user']['password'] = '$1$HhgEQ4ug$CA4/Aqm.LDZuFvtAO4Qu8.' # openssl passwd -1 "devuser"
 
+default['devenv']['zshd']['repo'] = 'https://github.com/byplayer/dot.zsh.d.git'
+default['devenv']['zshd']['ref'] = 'master'
+
 # tmux
 default['tmux']['install_method'] = 'source'
 default['tmux']['configure_options'] = %w(--prefix=/opt/tmux)
