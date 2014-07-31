d9b03dcacf5099a0fd73ba5433c24ec6dd182a06
Yukio Goto <yukio.goto@mail.rakuten.com>
2014-07-31 09:33:00 +0800

    change arignment

--- recipes/_packages.rb
+++ recipes/_packages.rb
@@ -1,21 +1,21 @@
 %w(aptitude
-  gnome-terminal
-  lubuntu-desktop zsh autotools-dev automake libtool
-  libevent-dev libncurses5-dev zlib1g-dev libbz2-dev libyaml-dev
-  libxml2-dev libxslt1-dev libreadline-dev xclip patch
-  chromium-browser
-  wget ncftp
-  ibus-mozc mozc-server mozc-utils-gui
-  exuberant-ctags
-  emacs24 emacsen-common ibus-el rdtool-elisp emacs-goodies-el
-  xfonts-shinonome python-xlib
-  libterm-readkey-perl
-  mlocate ssh
-  libsqlite3-dev sqlite3
-  expect dkms
-  eblook lookup-el
-  libgecode-dev
-  python-sphinx python-pip
+   gnome-terminal
+   lubuntu-desktop zsh autotools-dev automake libtool
+   libevent-dev libncurses5-dev zlib1g-dev libbz2-dev libyaml-dev
+   libxml2-dev libxslt1-dev libreadline-dev xclip patch
+   chromium-browser
+   wget ncftp
+   ibus-mozc mozc-server mozc-utils-gui
+   exuberant-ctags
+   emacs24 emacsen-common ibus-el rdtool-elisp emacs-goodies-el
+   xfonts-shinonome python-xlib
+   libterm-readkey-perl
+   mlocate ssh
+   libsqlite3-dev sqlite3
+   expect dkms
+   eblook lookup-el
+   libgecode-dev
+   python-sphinx python-pip
 ).each do |pkg|
   package pkg do
     action :install
