a6dfe605a697ac4156130e4379d932428f1de0d6
byplayer <byplayer100@gmail.com>
2015-12-18 12:27:21 +0800

    fix locale bug

--- recipes/_locales.rb
+++ recipes/_locales.rb
@@ -1,7 +1,7 @@
-# TODO: use locales package
 bash 'locale' do
   code <<-EOH
-    locale-gen node['devenv']['locale']
+    locale-gen #{node['devenv']['locale']}
+    localectl set-locale LANG="#{node['devenv']['locale']}"
     dpkg-reconfigure locales
   EOH
 end
