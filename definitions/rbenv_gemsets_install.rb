d3ba937e9be1766fe6412bf17c91f94aec5c06bb
byplayer <byplayer100@gmail.com>
2016-04-11 10:52:18 +0800

    fix recipe

--- definitions/rbenv_gemsets_install.rb
+++ definitions/rbenv_gemsets_install.rb
@@ -6,17 +6,20 @@ define :install_rbenv_gemset,
   :gems => [] do
   Chef::Log.info("install gem set #{params[:ruby_ver]} " +
                  "#{params[:gemset_name]} [#{params[:gems].join(',')}]")
-  user params[:user]
-  cwd '/tmp'
-  code <<-SH
-    tmp_dir=`mktemp -d rbenv_gemset_tmp.XXXXXXXXXX`
-    pushd $tmp_dir
-    rbenv gemset create #{params[:ruby_ver]} #{params[:gemset_name]}
-    rbenv local #{params[:ruby_ver]}
-    rbenv gemset init #{params[:gemset_name]}
-    gem install #{params[:gems].join(' ')}
-    rbenv rehash
 
-    rm -r $tmp_dir
-  SH
+  bash "install_rbenv_gemset[#{params[:name]}]" do
+    user params[:user]
+    cwd '/tmp'
+    code <<-SH
+      tmp_dir=`mktemp -d rbenv_gemset_tmp.XXXXXXXXXX`
+      pushd $tmp_dir
+      rbenv gemset create #{params[:ruby_ver]} #{params[:gemset_name]}
+      rbenv local #{params[:ruby_ver]}
+      rbenv gemset init #{params[:gemset_name]}
+      gem install #{params[:gems].join(' ')}
+      rbenv rehash
+
+      rm -r $tmp_dir
+    SH
+  end
 end
--- recipes/_rbenv.rb
+++ recipes/_rbenv.rb
@@ -8,16 +8,14 @@ node['rbenv']['rubies'].each do |ver|
   rbenv_ruby ver
 end
 
-bash 'rbenv gemsets install' do
-  if node['rbenv']['gemsets']
-    node['rbenv']['gemsets'].each do |ver, sets|
-      sets.each do |gemset_name, gems|
-        install_rbenv_gemset "#{ver}@#{gemset_name}" do
-          user node['rbenv']['user']
-          ruby_ver ver
-          gemset_name gemset_name
-          gems gems
-        end
+if node['rbenv']['gemsets']
+  node['rbenv']['gemsets'].each do |ver, sets|
+    sets.each do |gemset_name, gems|
+      install_rbenv_gemset "#{ver}@#{gemset_name}" do
+        user node['rbenv']['user']
+        ruby_ver ver
+        gemset_name gemset_name
+        gems gems
       end
     end
   end
