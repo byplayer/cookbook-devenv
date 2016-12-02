441fa9e26bb8f65ed0ad8e95d932a3dbdb91e3aa
Yukio Goto <yukio.goto@rakuten.com>
2016-12-02 14:11:13 +0800

    add virtual env and python3

--- recipes/_packages.rb
+++ recipes/_packages.rb
@@ -20,6 +20,9 @@
    libffi-dev
    eblook lookup-el
    python-sphinx python-pip
+   python-virtualenv
+   python3-sphinx python3-pip
+   python3-virtualenv
    postgresql postgresql-client libpq-dev
    manpages-dev
    iftop bmon iptraf nload nethogs speedometer slurm
