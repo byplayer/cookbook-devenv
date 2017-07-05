8d381615d4382b63f0c64c9f8349e66ae3e36aea
byplayer <byplayer100@gmail.com>
2017-07-05 09:11:46 +0800

    support ubuntu 14

--- recipes/_timezone.rb
+++ recipes/_timezone.rb
@@ -1,5 +1,13 @@
-bash 'set_timezone' do
-  code <<-EOH
-    /usr/bin/timedatectl --no-ask-password set-timezone #{node['tz']}
-  EOH
+if node['platform_version'].to_i >= 16
+  bash 'set_timezone' do
+    code <<-EOH
+      /usr/bin/timedatectl --no-ask-password set-timezone #{node['tz']}
+    EOH
+  end
+else
+  bash 'set_timezone' do
+    code <<-EOH
+      /usr/bin/timedatectl set-timezone #{node['tz']}
+    EOH
+  end
 end
