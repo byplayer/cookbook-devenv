a15157749843d5e5d38a4616ede9d7fb6c53fc8c
byplayer <byplayer100@gmail.com>
2017-09-27 08:48:17 +0800

    Update _locales.rb

--- recipes/_locales.rb
+++ recipes/_locales.rb
@@ -2,6 +2,6 @@ bash 'locale' do
   code <<-EOH
     locale-gen #{node['devenv']['locale']}
     localectl set-locale LANG="#{node['devenv']['locale']}"
-    dpkg-reconfigure locales -u LANG="#{node['devenv']['locale']}"
+    dpkg-reconfigure --frontend=nointeractive locales
   EOH
 end
