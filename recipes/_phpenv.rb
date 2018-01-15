7ab21862023daf2ed602e38e2d106d4d5814c987
byplayer <byplayer100@gmail.com>
2018-01-15 17:17:24 +0800

    support ubuntu16

--- recipes/_phpenv.rb
+++ recipes/_phpenv.rb
@@ -1,6 +1,7 @@
 # create symlink for php build
 link '/usr/include/curl' do
   to '/usr/include/x86_64-linux-gnu/curl'
+  only_if 'test -d /usr/include/x86_64-linux-gnu/curl'
 end
 
 %w[phpenv::default].each do |recipe|
