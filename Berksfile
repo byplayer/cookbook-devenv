f8a3e3b6f939b69ba626a64a06cac877e55eab0d
byplayer <byplayer100@gmail.com>
2018-01-22 16:56:58 +0800

    docker-machine

--- Berksfile
+++ Berksfile
@@ -1,5 +1,6 @@
 source "https://api.berkshelf.com"
 
 cookbook "the_silver_searcher", git: "https://github.com/byplayer/the_silver_searcher-cookbook.git"
+cookbook "docker-machine", git: "https://github.com/byplayer/chef-cookbook-docker-machine.git"
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures devenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w(tmux apt global docker docker_compose updatedb
+%w(tmux apt global docker docker_compose docker-machine updatedb
    the_silver_searcher apt-repo heroku-toolbelt
    setlayout golang rbenv trash-cli phpenv).each do |cookbook|
   depends cookbook
