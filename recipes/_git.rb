1a08cba912196faab1bed35593b4e281b6352768
byplayer <byplayer100@gmail.com>
2018-02-02 11:04:14 +0800

    follow link

--- recipes/_git.rb
+++ recipes/_git.rb
@@ -1,6 +1,6 @@
 bash 'install_git' do
   cwd '/usr/local/src'
   code <<-EOH
-    curl https://raw.github.com/byplayer/docs/master/git/git-install.sh | bash
+    curl -sL https://raw.github.com/byplayer/docs/master/git/git-install.sh | bash
   EOH
 end
