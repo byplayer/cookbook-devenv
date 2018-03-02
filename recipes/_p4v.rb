c63b24814973466ae4e540ede1857a1a623baee7
byplayer <byplayer100@gmail.com>
2018-03-02 11:33:25 +0800

    change p4v install path

--- recipes/_p4v.rb
+++ recipes/_p4v.rb
@@ -1,8 +1,9 @@
 bash 'install p4v' do
-  cwd '/usr/local'
-  creates "/usr/local/p4v-#{node['p4v']['version']}"
+  cwd '/opt'
+  creates "/opt/p4v-#{node['p4v']['version']}"
   code <<-EOH
     tar xzf /vagrant/apps/p4v-#{node['p4v']['version']}.tgz
+    chown -R root:root p4v-#{node['p4v']['version']}
 
     if [ -L p4v ];
     then
