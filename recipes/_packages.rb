3233d085b4ea27c155523f6549066f4084bc92ec
byplayer <byplayer100@gmail.com>
2018-03-08 11:09:04 +0800

    add inotify-tools

--- recipes/_packages.rb
+++ recipes/_packages.rb
@@ -1,5 +1,6 @@
 %W[aptitude
    gnome-terminal
+   inotify-tools
    lubuntu-desktop zsh autotools-dev automake libtool cmake
    libevent-dev libncurses5-dev zlib1g-dev libbz2-dev libyaml-dev
    libxml2-dev libxslt1-dev libreadline-dev xclip patch
