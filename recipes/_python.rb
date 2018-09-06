4bf910df50aebcecaa9e3df9332a1a2b0a1442c0
byplayer <byplayer100@gmail.com>
2018-09-06 13:30:43 +0800

    support anaconda install dir/user

--- recipes/_python.rb
+++ recipes/_python.rb
@@ -1,4 +1,7 @@
 node.default['conda']['user'] = node['devenv']['user']['name']
 node.default['conda']['group'] = node['devenv']['user']['name']
 
+node.default['install']['dir'] = '/opt/anaconda'
+node.default['install']['user'] = 'root'
+
 include_recipe 'conda'
