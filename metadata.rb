17f7bfa3d2329bfd2163686d0856804a96ecffe5
byplayer <byplayer100@gmail.com>
2018-10-31 00:33:47 +0800

    fix warning

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description      'Installs/Configures devenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w(tmux apt universal-ctags global docker docker_compose docker-machine updatedb
+%w[tmux apt universal-ctags global docker docker_compose docker-machine updatedb
    the_silver_searcher apt-repo heroku-toolbelt
-   setlayout golang rbenv trash-cli).each do |cookbook|
+   setlayout golang rbenv trash-cli].each do |cookbook|
   depends cookbook
 end
