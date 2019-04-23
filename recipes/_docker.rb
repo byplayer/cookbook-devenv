d8c35e7fd9f1d6303e36997fc2e6597eb6a88fa2
byplayer <byplayer100@gmail.com>
2019-04-23 15:23:47 +0800

    use stable(default)

--- recipes/_docker.rb
+++ recipes/_docker.rb
@@ -1,7 +1,7 @@
-docker_installation_package 'default' do
-  repo_channel 'nightly'
-  package_version ''
-end
+# docker_installation_package 'default' do
+#   repo_channel 'nightly'
+#   package_version ''
+# end
 
 docker_service 'default' do
   action [:create, :start]
