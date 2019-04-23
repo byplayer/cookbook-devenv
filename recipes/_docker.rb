3548994bd08e9e591d15b2e62baca3a583016af6
byplayer <byplayer100@gmail.com>
2019-04-23 12:55:36 +0800

    set nightly for temporary to support 19.04

--- recipes/_docker.rb
+++ recipes/_docker.rb
@@ -1,6 +1,9 @@
+docker_installation_package do
+  repo_channel 'nightly'
+end
+
 docker_service 'default' do
   action [:create, :start]
 end
 
-include_recipe 'docker-machine'
 include_recipe 'docker_compose::installation'
