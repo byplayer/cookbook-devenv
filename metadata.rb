77bd12067f8c2ace6e473ea56f3ce63eb7ed27c2
byplayer <byplayer100@gmail.com>
2019-04-23 16:02:00 +0800

    skip docker temporary

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,9 @@ description      'Installs/Configures devenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w[tmux apt universal-ctags global docker docker_compose updatedb
+# docker docker_compose
+%w[tmux apt universal-ctags global
+   updatedb
    the_silver_searcher apt-repo heroku-toolbelt
    setlayout golang rbenv trash-cli kubectl].each do |cookbook|
   depends cookbook
