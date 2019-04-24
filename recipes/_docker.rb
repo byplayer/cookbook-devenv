b062538116a2bed02e56c0f3ede7359f33d7b2c3
byplayer <byplayer100@gmail.com>
2019-04-24 10:42:21 +0800

    activate nightly docker

--- recipes/_docker.rb
+++ recipes/_docker.rb
@@ -1,10 +1,10 @@
-# docker_installation_package 'default' do
-#   repo_channel 'test'
-#   package_version ''
-# end
+docker_installation_package 'default' do
+  repo_channel 'nightly'
+  package_version ''
+end
 
-# docker_service 'default' do
-#   action [:create, :start]
-# end
+docker_service 'default' do
+  action [:create, :start]
+end
 
-# include_recipe 'docker_compose::installation'
+include_recipe 'docker_compose::installation'
