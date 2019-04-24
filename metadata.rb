74097eddc9ff8b2f92d224f5712339cf6e8d9e9a
byplayer <byplayer100@gmail.com>
2019-04-24 11:19:22 +0800

    Revert "activate nightly docker"
    This reverts commit b062538116a2bed02e56c0f3ede7359f33d7b2c3.

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Installs/Configures devenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+# docker docker_compose
 %w[tmux apt universal-ctags global
    updatedb
-   docker docker_compose
    the_silver_searcher apt-repo heroku-toolbelt
    golang rbenv trash-cli kubectl].each do |cookbook|
   depends cookbook
