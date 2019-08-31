92cc3d6716e51a99ac9ab1ca54cd9649daafb419
byplayer <byplayer100@gmail.com>
2019-08-31 10:37:46 +0800

    :recycle: Refactor boost directory structure
    Don't use stage, but use install and install headers and libs and docs.

--- recipes/_boost.rb
+++ recipes/_boost.rb
@@ -6,23 +6,34 @@ remote_file "#{Chef::Config['file_cache_path']}/boost_#{node['boost']['version']
   not_if "test -f #{Chef::Config['file_cache_path']}/boost_#{node['boost']['version']&.tr('.', '_')}.tar.gz"
 end
 
+install_dir = File.join(node['boost']['install_base'], 'boost_' + node['boost']['version']&.tr('.', '_'))
 execute "Extracting and Building boost #{node['boost']['version']} from source" do
   cwd node['boost']['install_base']
+  cwd Chef::Config['file_cache_path']
   command <<-COMMAND
-    if [ -L boost ]; then
-      rm boost
+    if [ -L #{File.join(node['boost']['install_base'], boost)} ]; then
+      rm #{File.join(node['boost']['install_base'], boost)}
+    fi
+
+    if [ -d #{boost_#{node['boost']['version']&.tr('.', '_')} ]; then
+      rm -r #{boost_#{node['boost']['version']&.tr('.', '_')}
     fi
 
     tar xzf #{Chef::Config['file_cache_path']}/boost_#{node['boost']['version']&.tr('.', '_')}.tar.gz
     cd boost_#{node['boost']['version']&.tr('.', '_')}
     ./bootstrap.sh --with-toolset=gcc
-    ./b2 toolset=gcc -link=static,shared runtime-link=shared threading=multi variant=release --stagedir="stage/gcc"
-    ./bootstrap.sh --with-toolset=clang
-    ./b2 toolset=clang -link=static,shared runtime-link=shared threading=multi variant=release --stagedir="stage/clang"
-    rm -r bin.v2
-    cd ../
+    ./b2 --prefix=#{install_dir} headers
+    ./b2 toolset=gcc -link=static,shared runtime-link=shared threading=multi variant=release --prefix=#{install_dir} --libdir=#{File.join(install_dir, '/lib/gcc')} install
+    rm -r stage
+    ./b2 toolset=clang -link=static,shared runtime-link=shared threading=multi variant=release --prefix=#{install_dir} --libdir=#{File.join(install_dir, '/lib/clang')} install
+
+    cp boost.png #{install_dir}/
+    cp index.html #{install_dir}/
+    cp -r doc #{install_dir}/
+    cp -r libs #{install_dir}/
+    cd #{node['boost']['install_base']}
     ln -s boost_#{node['boost']['version']&.tr('.', '_')} boost
   COMMAND
 
-  not_if "test -d #{File.join(node['boost']['install_base'], 'boost_' + node['boost']['version']&.tr('.', '_'))}"
+  not_if "test -d #{install_dir}"
 end
