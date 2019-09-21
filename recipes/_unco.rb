f1365a1d1d4861b2fcf7f9b8b865abca7173f8a4
byplayer <byplayer100@gmail.com>
2019-09-22 01:15:23 +0800

    :+1: run "make clean" after unco install
    Cleaning up work directory

--- recipes/_unco.rb
+++ recipes/_unco.rb
@@ -12,6 +12,7 @@ execute 'install fzf' do
     cmake .
     make preinstall
     cmake -DCMAKE_INSTALL_PREFIX=#{node['unco']['install_target']} -P cmake_install.cmake
+    make clean
   COMMAND
 
   not_if "#{node['unco']['install_target']}/bin/unco --version | grep #{node['unco']['version']}"
