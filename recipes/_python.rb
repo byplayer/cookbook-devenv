fdaca174c6893de430ca0235464d81085bba3819
byplayer <byplayer100@gmail.com>
2020-02-29 01:50:12 +0900

    :+1: Add pyenv virtualenv

--- recipes/_python.rb
+++ recipes/_python.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 pyenv_user_install node['devenv']['user']['name']
+
+pyenv_plugin 'virtualenv' do
+  git_url     'https://github.com/pyenv/pyenv-virtualenv.git'
+  git_ref     'master'
+  user        node['devenv']['user']['name']
+end
