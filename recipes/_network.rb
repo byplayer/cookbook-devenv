8bc285ba9af90ae666ac177ca09308596ab1c62d
Yukio Goto <yukio.goto@rakuten.com>
2020-06-25 13:31:44 +0900

    :bug: remove resolved.conf template
    If I set it then I can't solve domain name.
    
    I think it is affected by DNS=192.168.39.3.
    It may not be necessary on ubuntu 2004.

--- recipes/_network.rb
+++ recipes/_network.rb
@@ -1,14 +1,11 @@
-template "/etc/nsswitch.conf" do
+# frozen_string_literal: true
+
+template '/etc/nsswitch.conf' do
   source 'etc/nsswitch.conf.erb'
   action :create
 end
 
-template "/etc/systemd/resolved.conf" do
-  source 'etc/systemd/resolved.conf.erb'
-  action :create
-end
-
-template "/etc/ntp.conf" do
+template '/etc/ntp.conf' do
   source 'etc/ntp.conf.erb'
   action :create
 end
